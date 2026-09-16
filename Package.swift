2aaece48c790e636ff2419c8734e8fd2d3c91013
yaozhizhong <mchael.yao@tradplus.com>
2026-09-16 15:58:14 +0800

    Release 15.15.0

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/tradplus/TradPlusAdSDK-SPM.git",
-            .exact("15.14.0")
+            .exact("15.15.0")
         ),
         .package(
             url: "https://github.com/vervegroup/hybid-ios-spm-sdk.git",
-            .exact("3.8.0")
+            .exact("3.9.1")
         ),
     ],
     targets: [
@@ -36,8 +36,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "TPVerveAdapter",
-            url: "https://github.com/tradplus/TradPlusAdSDK-SPM-HyBid/releases/download/15.14.0/TPVerveAdapter-15.14.0.xcframework.zip",
-            checksum: "1e6bb14d18d51872a1b639e1f8e0ffcaa00d69742bb5caac01c7442ba930bb92"
+            url: "https://github.com/tradplus/TradPlusAdSDK-SPM-HyBid/releases/download/15.15.0/TPVerveAdapter-15.15.0.xcframework.zip",
+            checksum: "8a0d3eb0e1455ff082dae57846b7a939618236b10e8d6408c921e0c53f1a05ff"
         ),
     ]
 )
